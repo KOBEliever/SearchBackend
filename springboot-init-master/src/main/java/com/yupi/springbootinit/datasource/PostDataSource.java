dc1612d6b69c92c673db4f169b397b8bb9c5e03a
DingYi <970102437@qq.com>
2023-10-18 18:09:06 +0800

    从RequestContextHolder中获取request

--- springboot-init-master/src/main/java/com/yupi/springbootinit/datasource/PostDataSource.java
+++ springboot-init-master/src/main/java/com/yupi/springbootinit/datasource/PostDataSource.java
@@ -6,8 +6,11 @@ import com.yupi.springbootinit.model.entity.Post;
 import com.yupi.springbootinit.model.vo.PostVO;
 import com.yupi.springbootinit.service.PostService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @Service
 public class PostDataSource implements DataSource<PostVO>{
@@ -19,7 +22,9 @@ public class PostDataSource implements DataSource<PostVO>{
         postQueryRequest.setSearchText(searchText);
         postQueryRequest.setPageSize(pageSize);
         postQueryRequest.setCurrent(pageNum);
-        Page<PostVO> postPageVO = postService.listPostVOByPage(postQueryRequest,null);
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = servletRequestAttributes.getRequest();
+        Page<PostVO> postPageVO = postService.listPostVOByPage(postQueryRequest,request);
         return postPageVO;
     }
 }
